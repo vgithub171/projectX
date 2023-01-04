3e1bc740ad39ff74cf53cbc42cd20920163b870e
Vaibhav Shinde <vdshinde36@gmail.com>
2023-01-04 15:29:25 +0530

    function added 4 and deleted some code

--- addnumber.js
+++ addnumber.js
@@ -4,9 +4,16 @@ function addNumber () {
 }
 
 function addNumber3 () {
-    return 2+10;
+    return 2+11;
 }
 
 function addNumber2 () {
 return 1+2;
+}
+
+
+
+
+function addNumber4() {
+    return 9+10;
 }
